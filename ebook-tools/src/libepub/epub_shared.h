0b7fd3112cae720093edbc87aa1b33da7de07792
nakee <Ely Levy (elylevy@cs.huji.ac.il)>
2008-05-30 13:42:20 +0000

    added documentation

--- ebook-tools/src/libepub/epub_shared.h
+++ ebook-tools/src/libepub/epub_shared.h
@@ -1,36 +1,45 @@
 #ifndef EPUB_SHARED_H
 #define EPUB_SHARED_H 1
 
+/**
+   Metadata types
+ */
 enum epub_metadata {
-  EPUB_ID,
-  EPUB_TITLE,
-  EPUB_CREATOR,
-  EPUB_CONTRIB,
-  EPUB_SUBJECT,
-  EPUB_PUBLISHER,
-  EPUB_DESCRIPTION,
-  EPUB_DATE,
-  EPUB_TYPE,
-  EPUB_FORMAT,
-  EPUB_SOURCE,
-  EPUB_LANG,
-  EPUB_RELATION,
-  EPUB_COVERAGE,
-  EPUB_RIGHTS,
-  EPUB_META,
+  EPUB_ID, /**< ebook id*/ 
+  EPUB_TITLE, /**< ebook title*/ 
+  EPUB_CREATOR, /**< ebook creator*/ 
+  EPUB_CONTRIB, /**< ebook contributor*/ 
+  EPUB_SUBJECT, /**< ebook subject*/ 
+  EPUB_PUBLISHER, /**< ebook publisher*/   
+  EPUB_DESCRIPTION, /**< ebook description*/ 
+  EPUB_DATE, /**< ebook data */ 
+  EPUB_TYPE, /**< ebook type */ 
+  EPUB_FORMAT, /**< ebook format */  
+  EPUB_SOURCE, /**< ebook source */ 
+  EPUB_LANG, /**<  ebook language */ 
+  EPUB_RELATION, /**< ebook relation*/  
+  EPUB_COVERAGE, /**< ebook coverage*/ 
+  EPUB_RIGHTS,/**< ebook rights */ 
+  EPUB_META, /**< ebook extra metadata*/ 
 };
 
+/**
+   Ebook Iterator types
+*/
 enum eiterator_type {
-  EITERATOR_SPINE,
-  EITERATOR_LINEAR,
-  EITERATOR_NONLINEAR,
+  EITERATOR_SPINE, /**< all the spine */  
+  EITERATOR_LINEAR, /**< the linear reading spine parts */
+  EITERATOR_NONLINEAR, /**< the non linear reading spine parts */
   //  EITERATOR_TOUR,
 };
 
+/**
+   Ebook Table Of Content Iterator types
+*/
 enum titerator_type {
-  TITERATOR_NAVMAP,
-  TITERATOR_GUIDE,
-  TITERATOR_PAGES,
+  TITERATOR_NAVMAP, /**< Navigation map  */
+  TITERATOR_GUIDE, /**< Guide to the ebook parts */
+  TITERATOR_PAGES, /**< The pages of the ebook */
 };
 
 #endif
