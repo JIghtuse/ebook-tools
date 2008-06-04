2bdf99c140eb0b0f98518f67d42cbf92126e62c7
nakee <Ely Levy (elylevy@cs.huji.ac.il)>
2008-06-04 18:39:05 +0000

    major cleanup added some missing features

--- ebook-tools/src/tools/einfo.c
+++ ebook-tools/src/tools/einfo.c
@@ -9,89 +9,124 @@ void quit(int code) {
 }
 
 void usage(int code) {
-    fprintf(stderr, "Usage: einfo [options] <filename>\n");
-    fprintf(stderr, "   -h\t Help message\n");
-    fprintf(stderr, "   -v\t Verbose (error)\n");
-    fprintf(stderr, "   -vv\t Verbose (warnings)\n");
-    fprintf(stderr, "   -vvv\t Verbose (info)\n");
-    fprintf(stderr, "   -d\t Debug mode (implies -vvv)\n");
-    fprintf(stderr, "   -p\t Linear print book (normal reading)\n");
-    fprintf(stderr, "   -pp\t Print the whole book\n");
-    fprintf(stderr, "   -t <tour id>\t prints the tour <tour id>\n");
-    fprintf(stderr, "   -g Print the guide file as html (if exists)\n");
-    fprintf(stderr, "   -n Print the toc ncx file as html (if exists)\n");
+  fprintf(stderr, "Usage: einfo [options] <filename>\n");
+  fprintf(stderr, "   -h\t Help message\n");
+  fprintf(stderr, "   -q\t don't print short meta info summary\n");
+  fprintf(stderr, "   -v\t Verbose (error)\n");
+  fprintf(stderr, "   -vv\t Verbose (warnings)\n");
+  fprintf(stderr, "   -vvv\t Verbose (info)\n");
+  fprintf(stderr, "   -d\t Debug mode (implies -vvv)\n");
+  fprintf(stderr, "   -p\t Linear print book (normal reading)\n");
+  fprintf(stderr, "   -pp\t Print the whole book\n");
+  fprintf(stderr, "   -t <tour id>\t prints the tour <tour id>\n");
 
-    exit(code);
+  exit(code);
 }
 
 int main(int argc , char **argv) {
   struct epub *epub;
   char *filename = NULL;
   char *tourId = NULL;
-  int verbose = 0, print = 0, debug = 0, ncx = 0, guide = 0;
+  int verbose = 0, print = 0, debug = 0, quiet = 0, tour = 0;
   
-  if (argc < 2) {
-      usage(3);
- }
-
   int i, j;
   
   for (i = 1;i<argc;i++) {
-      if (argv[i][0] == '-') {
+    loop:          
 
-        loop:          
-          for (j = 1;j<strlen(argv[i]);j++) {
-              switch(argv[i][j]) {
-                case 'v':
-                    verbose++;
-                    break;
-                case 'd':
-                    debug++;
-                    break;
-                case 'p':
-                    print++;
-                    break;
-                case 'h':
-                    usage(0);
-                    break;
-                case 'g':
-                    guide++;
-                    break;
-                case 'n': 
-                    ncx++;
-                    break;
-                case 't':
-                    i++;
-                    tourId = argv[i];
-                    goto loop;
-                    break;
-                default:
-                    usage(2);
-                    break;
-              }
+    if (argv[i][0] == '-') {
+
+      for (j = 1;j<strlen(argv[i]);j++) {
+        switch(argv[i][j]) {
+        case 'v':
+          verbose++;
+          break;
+        case 'd':
+          debug++;
+          break;
+        case 'q':
+          quiet++;
+          break;
+        case 'p':
+          print++;
+          break;
+        case 'h':
+          usage(0);
+          break;
+        case 't':
+          tour++;
+          if (tour > 1) {
+            fprintf(stderr, "Only one tour at a time is supported\n");
+            usage(2);
           }
 
-      } else {
-          if (! filename) {
-              filename = argv[i];
-          } else {
-              usage(1);
+          i++;
+          if (i<argc) {
+            tourId = argv[i];
+          } else {  
+            fprintf(stderr, "Missing tour id\n");
+            usage(2);
           }
+
+          i++;
+          goto loop;
+          break;
+        default:
+          fprintf(stderr, "Unknown flag %s\n", argv[i]);
+          usage(2);
+          break;
+        }
       }
+
+    } else {
+      if (! filename) {
+        filename = argv[i];
+      } else {
+        fprintf(stderr, "Missing file name\n");
+        usage(1);
+      }
+    }
   }
-  
+         
   if (! filename) {
+    fprintf(stderr, "Missing file name\n");
     usage(1);
   }
   
   if (debug)
-      verbose = 4;
+    verbose = 4;
   
   if (! (epub = epub_open(filename, verbose)))
     quit(1);
   
-  epub_dump(epub);
+  if (! quiet)
+    epub_dump(epub);
 
+  // Print the book
+  if (print > 0) {
+
+    struct eiterator *it;
+
+    if (print > 1) {
+      it = epub_get_iterator(epub, EITERATOR_LINEAR, 0);   
+    } else {
+      it = epub_get_iterator(epub, EITERATOR_SPINE, 0);
+    }
+    
+    do {
+      if (epub_it_get_curr(it)) {
+      
+        printf("%s\n", epub_it_get_curr(it));
+      }
+    } while (epub_it_get_next(it));
+
+    epub_free_iterator(it);
+
+  }
+  
+  if (tour) {
+    printf("Tours are still not supported\n");
+  }
 
   if (! epub_close(epub)) {
     quit(1);
