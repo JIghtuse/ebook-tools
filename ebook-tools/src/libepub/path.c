3e9f49fac63cb2f608e6380062d52607a107e626
Boris Egorov <egorov@linux.com>
2015-09-25 22:57:01 +0600

    path: Fix canonicalization - restore last slash positions

--- ebook-tools/src/libepub/path.c
+++ ebook-tools/src/libepub/path.c
@@ -8,6 +8,16 @@
 // TODO: simplify it?
 #define MAX_PATH_SIZE 4096
 
+/* returns last slash position in @s or -1 if there is no one */
+int get_last_slash_pos(char *s)
+{
+    int last_slash = -1;
+    char *slash = strrchr(s, '/');
+    if (slash)
+        last_slash = slash - s;
+    return last_slash;
+}
+
 char* canonicalize_filename(const char *str)
 {
   int prev_last_slash = -1;
@@ -27,12 +37,12 @@ char* canonicalize_filename(const char *str)
     switch (c) {
     case '/':
       if (cprev == '/' || j == 0) {
-        // skip entirely
+        // eat repeating and leading slashes
         ;
       } else {
         result[j++] = c;
         prev_last_slash = last_slash;
-        last_slash = i;
+        last_slash = j - 1;
       }
       break;
     case '.':
@@ -46,6 +56,16 @@ char* canonicalize_filename(const char *str)
         while (j > start_position)
           result[j--] = '\0';
         result[j] = '\0';
+
+        // we lost last slash positions, calculate them
+        prev_last_slash = -1;
+        last_slash = get_last_slash_pos(result);
+        if (last_slash != -1) {
+            // trying to find previous last slash position
+            result[last_slash] = ' ';
+            prev_last_slash = get_last_slash_pos(result);
+            result[last_slash] = '/';
+        }
       } else {
         // assume it is a valid to have dot in names
         result[j++] = c;
